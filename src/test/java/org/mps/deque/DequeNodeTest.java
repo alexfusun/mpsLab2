85d95e75e2e303bb1f8fc596e96b496af002dd3a
ElRusitoFeliz <montiel.fernandez.juanmanuel@gmail.com>
2023-03-09 12:10:06 +0100

    intento de pruebas

--- src/test/java/org/mps/deque/DequeNodeTest.java
+++ src/test/java/org/mps/deque/DequeNodeTest.java
@@ -1,7 +1,43 @@
 package org.mps.deque;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DequeNodeTest {
 
+    @BeforeEach
+    void setup() {
+        int item = 8;
+        DequeNode<Integer> nodo = new DequeNode<int>(item, null, null);
+    }
+
+    @Test
+    @DisplayName("")
+    void setItem(){
+        assertEquals(item, nodo.getItem());
+    }
+
+    @Test
+    @DisplayName("")
+    void getItem(){
+        DequeNode<T> nodo = new DequeNode<T>(ite
+
+        assertEquals()
+    }
+
+
+    @Test
+    @DisplayName("")
+    void setPrevious(DequeNode<T> esperado){
+        assertEquals(esperado, this.previous);
+    }
+
+    @Test
+    @DisplayName("")
+    void setNext(DequeNode<T> esperado){
+        assertEquals(esperado, this.next);
+    }
 }
