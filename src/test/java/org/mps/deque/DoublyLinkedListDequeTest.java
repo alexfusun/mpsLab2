5c312dd9f20e66ccce1358e4aea874bef1eb274a
Alex <alexfusun@gmail.com>
2023-03-10 13:17:06 +0100

    Comentarios en DoublyLinkedListDequeueTest

--- src/test/java/org/mps/deque/DoublyLinkedListDequeTest.java
+++ src/test/java/org/mps/deque/DoublyLinkedListDequeTest.java
@@ -24,6 +24,7 @@ class DoublyLinkedListDequeTest {
             list = null;
         }
 
+        //Checks that first correctly returns the last value prepended
         @Test
         @DisplayName("first() returns first element")
         void shouldReturnElementInFrontOfList() {
@@ -33,6 +34,7 @@ class DoublyLinkedListDequeTest {
             assertEquals(expected, returned);
         }
 
+        //Check that last returns last element appended
         @Test
         @DisplayName("last() returns last element")
         void shouldReturnElementInEndOfList() {
@@ -61,6 +63,7 @@ class DoublyLinkedListDequeTest {
             list = null;
         }
 
+        //Check that deleteFirst correctly deletes first element and assigns new first
         @Test
         @DisplayName("deleteFirst() should remove first element")
         void shouldRemoveElementInFrontOfList() {
@@ -73,8 +76,10 @@ class DoublyLinkedListDequeTest {
             Integer returnedLast = list.last();
 
             assertEquals(expectedFirst, returnedFirst);
+            assertEquals(expectedLast, returnedLast);
         }
 
+        //Check that removeLast correctly deletes last element and assigns new last
         @Test
         @DisplayName("removeLast() returns last element")
         void shouldReturnElementInEndOfList() {
@@ -106,20 +111,23 @@ class DoublyLinkedListDequeTest {
             list = null;
         }
 
+        //When queue is empty, first raises an Exception
         @Test
         @DisplayName("first() method should raise DoubleEndedQueueException if queue is empty")
         void shouldReturnExceptionWhenCallingFirstFromAnEmptyQueue() {
             assertThrows(DoubleEndedQueueException.class, () -> list.first());
         }
 
+        //When queue is empty, last raises an Exception
         @Test
         @DisplayName("last() method should raise DoubleEndedQueueException if queue is empty")
         void shouldReturnExceptionWhenCallingLastFromAnEmptyQueue() {
             assertThrows(DoubleEndedQueueException.class, () -> list.last());
         }
 
+        //When queue is empty, deleteFirst and deleteLast raise an Exception
         @Test
-        @DisplayName("deleteFirst() and deleteLast() should rais DoubleEndedQueueException if queue is emtpy")
+        @DisplayName("deleteFirst() and deleteLast() should raise DoubleEndedQueueException if queue is emtpy")
         void shouldReturnExceptionWhenDeletingFromAnEmptyQueue() {
             assertThrows(DoubleEndedQueueException.class, () -> list.deleteFirst());
             assertThrows(DoubleEndedQueueException.class, () -> list.deleteLast());
@@ -141,6 +149,7 @@ class DoublyLinkedListDequeTest {
             list = null;
         }
 
+        //Check that size returns 0 when queue is empty
         @Test
         @DisplayName("size() should return size correctly")
         void sizeOfAnEmptyQueueIsZero() {
@@ -155,6 +164,7 @@ class DoublyLinkedListDequeTest {
             assertEquals(expected, returned);
         }
 
+        //Check that size returns correct values after appending and prepending
         @Test
         @DisplayName("append() and prepend() should increase size by 1")
         void sizeAfterAppendOrPrependIncreasesOne() {
@@ -173,6 +183,7 @@ class DoublyLinkedListDequeTest {
             assertEquals(expected, returned);
         }
 
+        //Check that size correctly returns values after deleting
         @Test
         @DisplayName("deleteFirst() and deleteLast() should decrease size by 1")
         void sizeAfterDeleteDecreasesOne() {
